eb1227d918372e38b5f8ec3126cfcdc5137541c2
Gasparotto <mateusg@cpqd.com.br>
2021-06-20 22:30:36 -0300

    primeiro commit

---
+++ user_site/busca_pev/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>BUSCA PEV</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/busca_pev/styles/main.css">
+    <link rel="stylesheet" href="/busca_pev/styles/responsive.css">
+    <link rel="stylesheet" href="/busca_pev/styles/home.css">
+
+</head>
+
+<body>
+
+    <div id="page-home">
+
+        <div class="content">
+            <header>
+                <img src="/busca_pev/assets/logo.svg" alt="Logomarca">
+                <a href="#">
+                    <span>
+                    </span>
+                    Entrar / Se cadastrar
+                </a>
+            </header>
+            <main>
+                <h1>PEV</h1>
+                <p>Pontos de Entrega Voluntária de entulho e outros materiais recicláveis</p>
+                <a href="#"><span></span><strong>Pesquisar PEVs</strong></a>
+            </main>
+        </div>
+
+    </div>
+
+</body>
+
+</html>

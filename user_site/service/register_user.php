d5c946dbbb3584d78bca421e9b44c8a72dfcf4f8
renato vinicios <renato.v@aluno.ifsp.edu.br>
2021-08-11 18:34:24 -0300

    Ajustando para exibir user apos registro

--- user_site/service/register_user.php
+++ user_site/service/register_user.php
@@ -12,6 +12,8 @@ $password = md5(trim($_POST["password"]));
 $sql = "INSERT INTO PEV_USER (NAME, LAST_NAME, EMAIL, PASSWORD) VALUES ('$name', '$last_name', '$email', '$password')";
 
 if (mysqli_query($conn, $sql)) {
+	$result = mysqli_query($conn, "SELECT * FROM PEV_USER WHERE EMAIL = '$email'");
+	$data = mysqli_fetch_array($result);
 	$_SESSION["user_id"] = $data["ID"];
 	$_SESSION["user_name"] = stripslashes($data["NAME"]);
 	$_SESSION["user_last_name"] = stripslashes($data["LAST_NAME"]);
@@ -19,7 +21,7 @@ if (mysqli_query($conn, $sql)) {
 	header("Location: ../index.php");
 } else {
 	$_SESSION["register_response"] = "Já existe usuário com esse email";
-    header("Location: ../views/login.php");
+	header("Location: ../views/login.php");
 }
 
 mysqli_close($conn);
